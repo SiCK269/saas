a457bb42f43ee38122a2525e0d01e8baaeb0b073
alihaitham2014 <alihaitham2014@gmail.com>
2024-11-20 18:51:19 +0000

    Added arg DJANGO_SECRET_KEY dockerfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'visits'
+    'visits',
+    'command'
 ]
 
 MIDDLEWARE = [
@@ -135,10 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendor"
-STATICFILES_DIRS = [
-    STATICFILES_VENDOR_DIR
-]
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # source(s) for python collectstatic
 
